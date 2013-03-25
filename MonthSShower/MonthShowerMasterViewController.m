19888e7a0590dd10477fc8377bd52fd07660d7c7
Nazar Demchuk <Dazfree@gmail.com>
2013-03-25 18:50:24 +0200

    Revert "seasons in sections"
    This reverts commit 7a13ded136fc9ea6fdc560d9596eb0b530ed9e2d.

--- MonthSShower/MonthShowerMasterViewController.m
+++ MonthSShower/MonthShowerMasterViewController.m
@@ -81,8 +81,6 @@
     
     
 //    [MonthShowerMasterViewController:MonthShowerMasterViewController animated:YES];
-    
-    
 
     
 }
@@ -116,10 +114,20 @@
 
 
 
+
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return 3;
 }
+
+
+
+
+
+
+
+
 
 
 
@@ -164,56 +172,47 @@
     return YES;
 }
 */
--(NSUInteger)seasonNumberFromMonthNumber:(NSUInteger)monthNumber
-{
-        if (12 == monthNumber)
-            {
-                return 1;
-            }
-        else
-            {
-                return (monthNumber / 3);
-            }
-}
 
-
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
-{
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     
-//    NSArray *myArray = [[NSArray alloc] initWithObjects:@"", @"Январь", @"Февраль", @"Март", @"Апрель", @"Май", @"Июнь", @"Июль", @"Август", @"Сентябрь", @"Октябрь", @"Ноябрь",@"Декабрь", nil];
+    NSArray *myArray = [[NSArray alloc] initWithObjects:@"", @"Январь", @"Февраль", @"Март", @"Апрель", @"Май", @"Июнь", @"Июль", @"Август", @"Сентябрь", @"Октябрь", @"Ноябрь",@"Декабрь", nil];
+    
+    
     
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"M"];
     
-//#error тут ты выделяешь память для currentMonth, а потом присваиваешь указателю другое значение. тут можно было сделать просто NSString *currentMonth = [dateFormatter stringFromDate:[NSDate date]]. до введения ARC это было бы мемори ликом
-//    NSString *currentMonth = [[NSString alloc]init];
-//    currentMonth = [dateFormatter stringFromDate:[NSDate date]];
-    
-    NSString *currentMonth = [dateFormatter stringFromDate:[NSDate date]];
+#error тут ты выделяешь память для currentMonth, а потом присваиваешь указателю другое значение. тут можно было сделать просто NSString *currentMonth = [dateFormatter stringFromDate:[NSDate date]]. до введения ARC это было бы мемори ликом
+    NSString *currentMonth = [[NSString alloc]init];
+    currentMonth = [dateFormatter stringFromDate:[NSDate date]];
     NSInteger a = [currentMonth integerValue];
     
-   NSLog(@"The current Month is %d", a);
+    NSLog(@"The current Month is %@", [myArray objectAtIndex: a]);    
     
-    NSArray *seasons = [[NSArray alloc] initWithObjects:@"Winter", @"Spring", @"Summer", @"Autumn", @"Winter",nil];
     
-//#error в obj-c есть оператор ",", но он используется крайне редко, и делает совсем не то что ты хотел тут получить. в твоем случае нужно использовать логическое или "||", и тогда условие будет выглядеть так if (a == 1 || a == 2 || a == 12)
-//#error причина креша состоит вот в чем: текущий месяц март, поэтому на этом шаге а == 3. твое условие вычисляет а == 1 и получает false (3 != 1). дальше оно берет и просто вычислет 2, что в C дает true, потом 12 тоже дает true. в итоге каким бы ни было а, программ все равно всегда заходит в первый if. дальше оно смотрит section == 0. делегатские вызовы не всегда работают последовательно. т.е. табличка может сначала попросить хеде для section == 3, потом для section == 1 и т.д. в общем порядок не определен. ошибка в том что ты везде проверяешь section == 0. и получается, например, параметром в метод приходит section == 3, оно заходит в этот if и сравнивает section и 0, конечено же выходит false, поэтому не срабатывает return @"Winter". так как больше ни в какие else if оно не заходит, то выходит что метод ничего не возвращает, и из-за этого креш
-//   
-    NSUInteger currentSeasonNumber = [self seasonNumberFromMonthNumber: a];
+#error в obj-c есть оператор ",", но он используется крайне редко, и делает совсем не то что ты хотел тут получить. в твоем случае нужно использовать логическое или "||", и тогда условие будет выглядеть так if (a == 1 || a == 2 || a == 12)
+#error причина креша состоит вот в чем: текущий месяц март, поэтому на этом шаге а == 3. твое условие вычисляет а == 1 и получает false (3 != 1). дальше оно берет и просто вычислет 2, что в C дает true, потом 12 тоже дает true. в итоге каким бы ни было а, программ все равно всегда заходит в первый if. дальше оно смотрит section == 0. делегатские вызовы не всегда работают последовательно. т.е. табличка может сначала попросить хеде для section == 3, потом для section == 1 и т.д. в общем порядок не определен. ошибка в том что ты везде проверяешь section == 0. и получается, например, параметром в метод приходит section == 3, оно заходит в этот if и сравнивает section и 0, конечено же выходит false, поэтому не срабатывает return @"Winter". так как больше ни в какие else if оно не заходит, то выходит что метод ничего не возвращает, и из-за этого креш
+    if (a == 1, 2, 12)
+    {
+        if (section == 0)
+            return @"Winter"; 
+    }
+    else if (a == 3, 4, 5)
+    {
+        if (section == 0)
+            return @"Spring";
+    }
+    else if (a == 6, 7, 8)
+    {
+        if (section == 0)
+            return @"Summer";
+    }
+    else if (a == 9, 10, 11)
+    {
+        if (section == 0)
+            return @"Autumn";
+    }
     
-    if (section == 0)
-        {
-            return  [seasons objectAtIndex: currentSeasonNumber];
-        
-        }
-        else
-        {
-            NSUInteger currentSectionNumber = (currentSeasonNumber + (section - currentSeasonNumber) + 3 - currentSeasonNumber - 1);
-            return [seasons objectAtIndex: currentSectionNumber];
-        }
-}
-    
-
     
     
         
@@ -225,15 +224,15 @@
 //        return @"Winter";
 //    else
 //        return @"Spring";
-//}
+}
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-//    if ([[segue identifier] isEqualToString:@"showDetail"]) {
-//        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+    if ([[segue identifier] isEqualToString:@"showDetail"]) {
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
      //   NSDate *object = _objects[indexPath.row];
     //    [[segue destinationViewController] setDetailItem:object];
-   // }
+    }
 }
 
 @end
